027d97d79522d71baa135f94c6ac3a684d7e982e
StephyStuff <stephanie.birch13@gmail.com>
2016-10-28 12:20:06 -0400

    Created function for submitting form. Verified when pressing enter key, value is retained and creates a new list item.

--- js/data.js
+++ js/data.js
@@ -1,5 +1,29 @@
 (function(){
-  console.log('working');
+  window.dom = window.dom || {};
+
+  document.querySelector('form').addEventListener('submit', function pressEnter (e){
+    e.preventDefault();
+  console.log(e);
+  console.log(e.target.childNodes[1].value);
+
+      var newItem = document.createElement('li');
+      newItem.innerText = e.target.childNodes[1].value;
+      console.log('hello');
+      newItem.classList.add('new-item');
+      document.querySelector('ul').appendChild(newItem);
+      console.log('hey');
+    })
+
+
+
+
+
+
+
+
+  });
+
+
 
 
 

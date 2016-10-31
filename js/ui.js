eba07fcbc9d9a5f510bd5529458944e41878d0b0
StephyStuff <stephanie.birch13@gmail.com>
2016-10-31 08:45:59 -0400

    Previous commit did not include modifications to ui.js.

--- js/ui.js
+++ js/ui.js
@@ -8,13 +8,13 @@
     eventObj.preventDefault();
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check') {
       eventObj.target.parentNode.classList.add('completed');
-    }
+    } 
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
       eventObj.target.parentNode.classList.add('delete');
     }
   });
 
-  
+
 
 
 

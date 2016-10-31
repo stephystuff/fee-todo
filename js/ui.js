59e21332daf199f45f744af0bc604f83dddc6d70
StephyStuff <stephanie.birch13@gmail.com>
2016-10-31 08:41:23 -0400

    Tried a function to remove article from 'completed' class if item is unchecked. Function did not work.

--- js/ui.js
+++ js/ui.js
@@ -4,18 +4,17 @@
   // Function for marking an item complete and deleting item//
   document
   .querySelector('ul')
-  .addEventListener("click", function markComplete(eventObj){
+  .addEventListener('click', function markComplete(eventObj){
     eventObj.preventDefault();
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check') {
       eventObj.target.parentNode.classList.add('completed');
     }
-
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
       eventObj.target.parentNode.classList.add('delete');
     }
   });
-  
 
+  
 
 
 

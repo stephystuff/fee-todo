bbabcd372e1d50eac3be5cae85ff585a73277e54
StephyStuff <stephanie.birch13@gmail.com>
2016-10-30 22:35:42 -0400

    Forgot to physically move functions over to ui.js. Functions moved over and both js files saved.

--- js/data.js
+++ js/data.js
@@ -24,30 +24,5 @@
     document.querySelector('ul').appendChild(addLi);
   }
 
-  document.querySelector('form').addEventListener('submit', function submit(eventObj){
-    eventObj.preventDefault();
-    var newSubmission = document.querySelector('.new-todo').value;
-
-    newToDo(newSubmission)
-  });
-
-  document
-    .querySelector('ul')
-    .addEventListener("click", function markComplete(eventObj){
-      eventObj.preventDefault();
-      if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check')
-        eventObj.target.parentNode.classList.add('completed');
-
-      if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete')
-            eventObj.target.parentNode.classList.add('delete');
-  });
-
-
-
-
-
-
-
-
 
 })();
--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,23 @@
 (function(){
   window.dom = window.dom || {};
 
-  
+  document.querySelector('form').addEventListener('submit', function submit(eventObj){
+    eventObj.preventDefault();
+    var newSubmission = document.querySelector('.new-todo').value;
+
+    newToDo(newSubmission)
+  });
+
+  document
+    .querySelector('ul')
+    .addEventListener("click", function markComplete(eventObj){
+      eventObj.preventDefault();
+      if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check')
+        eventObj.target.parentNode.classList.add('completed');
+
+      if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete')
+            eventObj.target.parentNode.classList.add('delete');
+  });
 
 
 

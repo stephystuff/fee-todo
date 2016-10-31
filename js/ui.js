072a05c552161a2c8d96f40c93ddb1a775486184
StephyStuff <stephanie.birch13@gmail.com>
2016-10-31 08:51:00 -0400

    Changed classList on 'markComplete' function to toggle. In the else if, changed classList to remove so thatwhen button is clicked, it removes the article from the 'completed' class.

--- js/ui.js
+++ js/ui.js
@@ -7,14 +7,14 @@
   .addEventListener('click', function markComplete(eventObj){
     eventObj.preventDefault();
     if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check') {
-      eventObj.target.parentNode.classList.add('completed');
-    }
-    if (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
-      eventObj.target.parentNode.classList.add('delete');
+      eventObj.target.parentNode.classList.toggle('completed');
+    } else if
+      (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
+      eventObj.target.parentNode.remove();
     }
   });
 
-  
+
 
 
 
